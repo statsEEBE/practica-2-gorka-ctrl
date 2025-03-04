fb870a54afae3c4f928621cb8f1463d6026359f9
gorka-ctrl <gorka.espinosa@estudiantat.upc.edu>
2025-03-04 09:47:19 +0100

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,19 @@
 #Codigo para problema 1
+
+
+#Coeficiente de determinación
+Rsq <- 0.1923; Rsq
+
+#Coeficiente de correlación
+R <- sqrt(Rsq); R
+
+#Variacion del valor de mercado = m
+m <- 0.06575; m
+
+#y=m*647.1+b(predicción)
+
+b <- 11.79755;b
+ypred <- m*647.1+b; ypred
+
+#Diagramas de dispersión = es b, ya que es el que tiene mas lógica
+
